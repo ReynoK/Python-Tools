4418b73b1928a4b6e4d27b87319658168869661f
reyno <reynokang@gmail.com>
2017-04-07 22:24:37 +0800

    增加验证函数

--- Crontab.py
+++ Crontab.py
@@ -1,11 +1,10 @@
 import re
-import pprint
 
 class Crontab(object):
 
-    every_dash = re.compile(r'\*/(\d+)')     # */n
-    range_dash = re.compile(r'(\d+)-(\d+)/(\d+)') #i-j/n
-    range_ = re.compile(r'(\d+)-(\d+)')  #i-j        优先级依次降低
+    every_dash = re.compile(r'\*/(\d+)')            # */n
+    range_dash = re.compile(r'(\d+)-(\d+)/(\d+)')   #i-j/n
+    range_ = re.compile(r'(\d+)-(\d+)')             #i-j 优先级依次降低
 
     def __init__(self, minute='*', hour='*', day='*', week='*', month='*'):
 
@@ -72,13 +71,24 @@ class Crontab(object):
             self._crontab_setting[label] = sorted(list(collect))
 
     def validate_datetime(self, date_time):
-        pass
+        _, month, day, hour, minute, _, week, _, _ = date_time.timetuple()
+
+        # 在datetime中,week=0表示星期一,这里要转换
+        week = (week + 1) % 7
+
+        for label,value in zip(['M','d','h','m','w'],[month,day,hour,minute,week]):
+            if value not in self._crontab_setting[label]:
+                return False
+
+        return True
 
 if __name__ == '__main__':
 
     crontab = Crontab(month='1-12/3')
-    pprint.pprint(crontab._crontab_setting['M'])
+    import datetime
 
+    print(crontab.validate_datetime(datetime.datetime.strptime('2017-04-03','%Y-%m-%d')))
+    print(crontab.validate_datetime(datetime.datetime.strptime('2017-05-02', '%Y-%m-%d')))
 
 
 
